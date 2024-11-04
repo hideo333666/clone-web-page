68dd72a29a3b43c1c77c8c2a75401fd834df887f
hideo333666 <muramura031409@gmail.com>
2024-11-04 22:26:14 +0900

    header追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header'
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -7,6 +8,7 @@ import Contact from './pages/Contact';
 const App: React.FC = () => {
   return (
     <Router>
+      <Header />
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Home: React.FC = () => {
-  return <h1>Home Page</h1>;
+const Header: React.FC = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
 };
 
-export default Home;
+export default Header;
